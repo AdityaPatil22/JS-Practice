b75609db5f4215ff850286734248d7e2be64278f
Rutujakirad <rkirad@redhat.com>
2025-07-23 12:56:40 +0530

    Code:Function to Reverse a String

--- 1. Basic/2.js
+++
@@ -1,9 +0,0 @@
-//2. Check if a string is a palindrome.
-
-const palindrome = (str) => {
-    const revstr = str.split('').reverse().join();
-    const result = revstr === str ? "true" : "flase";
-    console.log(result)
-}
-
-palindrome("aaaa")
